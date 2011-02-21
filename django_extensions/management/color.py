01e5deeedaafef22bed2047d882c557a0dd8cc4d
SeniorDevCode <senior.dev.software@gmail.com>
2011-02-21 17:39:23 -0500

    fixes passwd: return a single string, rather than a tuple

--- django_extensions/management/color.py
+++ django_extensions/management/color.py
@@ -7,9 +7,8 @@ from django.utils import termcolors
 
 def color_style():
     style = color.color_style()
-    if color.supports_color():
-        style.URL = termcolors.make_style(fg='green', opts=('bold',))
-        style.MODULE = termcolors.make_style(fg='yellow')
-        style.MODULE_NAME = termcolors.make_style(opts=('bold',))
-        style.URL_NAME = termcolors.make_style(fg='red')
+    style.URL = termcolors.make_style(fg='green', opts=('bold',))
+    style.MODULE = termcolors.make_style(fg='yellow')
+    style.MODULE_NAME = termcolors.make_style(opts=('bold',))
+    style.URL_NAME = termcolors.make_style(fg='red')
     return style
